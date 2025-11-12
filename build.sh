4bd788686b6ac2ea73d3f2fd92796e52a238157b
Surya <80670534+suryaln@users.noreply.github.com>
2025-11-13 01:00:16 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ docker login -u $DOCKER_USER -p $DOCKER_PASS
 #building the image:
 docker build -t nodejs:web .
 docker images
-
+ 
 #stopping the already running container:
 #docker stop nodejs
 #docker rm nodejs
