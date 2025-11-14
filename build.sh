ea657fac016d6dad3d16759f3269e463c16415a2
Surya <80670534+suryaln@users.noreply.github.com>
2025-11-14 13:41:43 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #login into dockerhub:
-docker login -u $DOCKER_USER -p $DOCKER_PASS
+#docker login -u $DOCKER_USER -p $DOCKER_PASS
  
 #building the image:
 docker build -t nodejs:web .
